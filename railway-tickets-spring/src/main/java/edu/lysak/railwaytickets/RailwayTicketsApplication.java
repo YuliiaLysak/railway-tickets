43acd37308df5c4317932c77495b35f6f91d0947
Yuliia Lysak <julia.m.lysak@gmail.com>
2021-04-18 13:53:38 +0300

    Initial project structure

---
+++ railway-tickets-spring/src/main/java/edu/lysak/railwaytickets/RailwayTicketsApplication.java
@@ -0,0 +1,12 @@
+package edu.lysak.railwaytickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayTicketsApplication.class, args);
+    }
+}
