a1525932ee990dbe8f1377291209418b672ff20e
Yuliia Lysak <julia.m.lysak@gmail.com>
2021-05-05 21:33:43 +0300

    Finished js for selecting the route data for editing by ADMIN

--- railway-tickets-spring/src/main/resources/static/admin/routes.main.js
+++ railway-tickets-spring/src/main/resources/static/admin/routes.main.js
@@ -16,23 +16,16 @@ function refreshRoutesData(selectedRouteId) {
         .then(renderedRoutes => $('.routes-list')[0].innerHTML = renderedRoutes)
 
         // method for handling selection of route
-        // TODO - finish this method (id autogeneration etc)
         .then(() => {
-            $('.accordion-item').click(function (event) {
-                event.preventDefault();
-                // TODO - remove/add class show, collapsed
-                // $('.accordion-collapse').each(function () {
-                //     $(this).removeClass('show');
-                // });
-                let $selectedItem = $(event.target);
-                // $selectedItem.addClass('show');
-                $('#routeIdId')[0].value = $selectedItem.data('route-id');
+            $('.btn-route').click(function (event) {
+                let $selectedItem = $(event.currentTarget);
+                $('#routeId')[0].value = $selectedItem.data('route-id');
                 $('#departureStationCity')[0].value = $selectedItem.data('route-departure-station-city');
                 $('#departureStationName')[0].value = $selectedItem.data('route-departure-station-name');
                 $('#arrivalStationCity')[0].value = $selectedItem.data('route-arrival-station-city');
                 $('#arrivalStationName')[0].value = $selectedItem.data('route-arrival-station-name');
                 $('#departureTime')[0].value = $selectedItem.data('route-departure-time');
-                $('#arrivalTime')[0].value = $selectedItem.data('arrival-time');
+                $('#arrivalTime')[0].value = $selectedItem.data('route-arrival-time');
                 $('#trainName')[0].value = $selectedItem.data('route-train-name');
                 $('#totalSeats')[0].value = $selectedItem.data('route-total-seats');
                 $('#pricePerSeat')[0].value = $selectedItem.data('route-price-per-seat');
@@ -44,39 +37,45 @@ function renderRoutes(routes, selectedRouteId) {
     if (routes.length === 0) {
         return '<p class="fs-2 text-danger" style="text-align: center;">No available routes</p>';
     }
-    return routes.map(route => renderRouteLine(route, selectedRouteId))
+    return routes.map((route, index) => renderRouteLine(route, selectedRouteId, index))
         .reduce((a, b) => a + b, '');
 }
 
-function renderRouteLine(route, selectedRouteId) {
-    let headerClass = 'accordion-button';
+function renderRouteLine(route, selectedRouteId, index) {
+    let headerClass = 'accordion-button collapsed';
     let bodyClass = 'accordion-collapse collapse';
     if (selectedRouteId === route.id) {
-        headerClass += ' collapsed';
-        bodyClass += ' show';
+        headerClass = 'accordion-button';
+        bodyClass = 'accordion-collapse collapse show';
     }
-    return `<div class="accordion-item"
-                    data-route-id="${route.id}"
-                    data-route-departure-station-city="${route.departureStation.city}"
-                    data-route-departure-station-name="${route.departureStation.name}"
-                    data-route-arrival-station-city="${route.arrivalStation.city}"
-                    data-route-arrival-station-name="${route.arrivalStation.name}"
-                    data-route-departure-time="${route.departureTime}"
-                    data-route-arrival-time="${route.arrivalTime}"
-                    data-route-train-name="${route.trainName}"
-                    data-route-total-seats="${route.totalSeats}"
-                    data-route-price-per-seat="${route.pricePerSeat}">
-                    <h2 class="accordion-header" id="headingOne">
-                        <button class="${headerClass}" type="button"
-                                data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                                aria-expanded="true" aria-controls="collapseOne">
-                            <span>${route.departureStation.city} (${route.departureStation.name})
-                             - ${route.arrivalStation.city} (${route.arrivalStation.name})</span>
-                            <span>${route.departureTime}</span>
+
+    let headingId = 'heading' + index;
+    let collapseId = 'collapse' + index;
+
+    return `<div class="accordion-item">
+                    <h2 class="accordion-header" id="${headingId}">
+                        <button class="${headerClass} btn-route" type="button"
+                                data-bs-toggle="collapse" data-bs-target="#${collapseId}"
+                                aria-expanded="true" aria-controls="${collapseId}"
+                                data-route-id="${route.id}"
+                                data-route-departure-station-city="${route.departureStation.city}"
+                                data-route-departure-station-name="${route.departureStation.name}"
+                                data-route-arrival-station-city="${route.arrivalStation.city}"
+                                data-route-arrival-station-name="${route.arrivalStation.name}"
+                                data-route-departure-time="${route.departureTime}"
+                                data-route-arrival-time="${route.arrivalTime}"
+                                data-route-train-name="${route.trainName}"
+                                data-route-total-seats="${route.totalSeats}"
+                                data-route-price-per-seat="${route.pricePerSeat}">
+                            <div class="col-8">
+                                <div class="row ms-2">${route.departureStation.city} (${route.departureStation.name})</div>
+                                <div class="row ms-2">${route.arrivalStation.city} (${route.arrivalStation.name})</div>
+                            </div>
+                            <div class="col-3">${route.departureTime}</div>
                         </button>
                     </h2>
-                    <div id="collapseOne" class="${bodyClass}"
-                         aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div id="${collapseId}" class="${bodyClass}"
+                         aria-labelledby="${headingId}" data-bs-parent="#accordionExample">
                         <div class="accordion-body">
                             <table class="table">
                                 <tbody>
