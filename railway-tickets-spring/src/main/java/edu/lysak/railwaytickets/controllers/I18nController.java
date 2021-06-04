aae4864aa7f3eff5bbf1ce584886829ede27edd3
Yuliia Lysak <julia.m.lysak@gmail.com>
2021-06-05 00:38:09 +0300

    Fixed method for getting content of i18n resource file
    (fixed FileSystemNotFoundException in case of launching app via
    console java -jar...)

--- railway-tickets-spring/src/main/java/edu/lysak/railwaytickets/controllers/I18nController.java
+++ railway-tickets-spring/src/main/java/edu/lysak/railwaytickets/controllers/I18nController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 /**
  * Class is required for providing i18n properties to frontend
@@ -20,12 +19,15 @@ import java.nio.file.Paths;
 public class I18nController {
 
     @GetMapping("/i18n/{fileName}.properties")
-    public ResponseEntity<String> getI18nProperties(@PathVariable String fileName) throws IOException, URISyntaxException {
-        URL resource = I18nController.class.getClassLoader().getResource(fileName + ".properties");
+    public ResponseEntity<String> getI18nProperties(@PathVariable String fileName) {
+        InputStream resource = I18nController.class.getClassLoader().getResourceAsStream(fileName + ".properties");
         if (resource == null) {
             log.warn("No resources file {}.properties", fileName);
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(Files.readString(Paths.get(resource.toURI())));
+        String result = new BufferedReader(new InputStreamReader(resource))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        return ResponseEntity.ok(result);
     }
 }
